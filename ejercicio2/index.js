68107aca4d21b8d4ea8fdebc832460d8428d86cc
Juan Andrés Cuevas <juanandrescuevas14@gmail.com>
2022-03-25 13:50:59 -0400

    Ejercicio 2

---
+++ ejercicio2/index.js
@@ -0,0 +1,9 @@
+let texto = "Este es el ejercicio 2";                           
+
+function mostrarTexto(){
+  let texto = "Mensaje funcion"; 
+  console.log(texto)
+} 
+mostrarTexto(); 
+
+console.log(texto);
