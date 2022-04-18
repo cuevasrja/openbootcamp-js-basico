9213c2fbf8b908db2028c3de8c5fbb7a2bf3a356
Juan Andrés Cuevas <juanandrescuevas14@gmail.com>
2022-04-18 19:52:46 -0400

    [Corrección] Ejercicio 13 - Parte 1

--- ejercicio13/parte1/public/index.js
+++ ejercicio13/parte1/public/index.js
@@ -10,10 +10,17 @@
 //     console.log('El fondo del div cambio a azul');
 // }
 
+function randomColor() {
+    let n = Math.round(Math.random() * 16777215);
+    let hexColor = "#" + n.toString(16);
+    return hexColor;
+}
+
 $("#box").hover(function() {
-    $(this).css("background-color", "blue");
-    console.log('%cCambio de color a azul', "color: yellow");
+    const color = randomColor();
+    $(this).css("background-color", color);
+    console.log(`%cCambio de color a ${color}`, "color: yellow");
 }, function () {
-    $(this).css("background-color", "red");
-    console.log('%cCambio de color a rojo', "color: yellow");
+    $(this).css("background-color", "white");
+    console.log('%cCambio de color a: white', "color: yellow");
 })
